452f1f041e51cec10e6490fdc83adfcfaa0ee680
sami <samikaya89@gmail.com>
2025-02-01 16:33:07 +0300

    Create calculationLetterGrade.js

---
+++ calculationLetterGrade.js
@@ -0,0 +1,21 @@
+let midterm = Math.random() * 100;
+let final = Math.random() * 100;
+let finalGrade = midterm * 0.3 + final * 0.7;
+let letterGrade;
+
+if (finalGrade >= 90 && finalGrade <= 100) {
+    letterGrade = 'A';
+} else if (finalGrade >= 80 && finalGrade <= 89) {
+    letterGrade = 'B';
+} else if (finalGrade >= 70 && finalGrade <= 79) {
+    letterGrade = 'C';
+} else if (finalGrade >= 60 && finalGrade <= 69) {
+    letterGrade = 'D';
+} else if (finalGrade >= 50 && finalGrade <= 59) {
+    letterGrade = 'E';
+} else {
+    letterGrade = 'F';
+}
+
+console.log(midterm);
+console.log(final);
